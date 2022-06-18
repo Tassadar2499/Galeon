d2c38639c775b70bfea0f9f364d980e2aca5f59a
demond98 <demon.d.98@mail.ru>
2022-06-18 20:57:11 +0300

    Add link to postgress in docker-compose Change UserId from int to string; Change appsettings;

--- src/GaleonServer/GaleonServer.Models/Models/UserToGame.cs
+++ src/GaleonServer/GaleonServer.Models/Models/UserToGame.cs
@@ -4,7 +4,7 @@
 	{
 		public int Id { get; set; }
 		public int GameId { get; set; }
-		public int UserId { get; set; }
+		public string UserId { get; set; } = null!;
 		public Game Game { get; set; } = null!;
 		public User User { get; set; } = null!;
 	}
